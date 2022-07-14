01216a7bd710d143cf74ccef98453de6ef6e8633
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-07-13 23:22:08 -0300

    Primer comit, implemente los 3 HTML y sus variables en los archivos JS

---
+++ Crud-Fetch.js
@@ -0,0 +1,6 @@
+const d = document
+$table = d.querySelector(".crud-table")
+$form = d.querySelector(".crud-table")
+$title = d.querySelector(".crud-table")
+$template = d.getElementById(".crud-template").content
+$fragment = d.createDocumentFragment()
