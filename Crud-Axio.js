afe57bffb422211437a993cbe8f16a3cb73de2f5
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-07-15 15:00:20 -0300

    Se implemento Fetch, los metodos get, post, put y delete

--- Crud-Axio.js
+++ Crud-Axio.js
@@ -1,6 +1,6 @@
 const d = document
 $table = d.querySelector(".crud-table")
-$form = d.querySelector(".crud-table")
-$title = d.querySelector(".crud-table")
-$template = d.getElementById(".crud-template").content
+$form = d.querySelector(".crud-form")
+$title = d.querySelector(".crud-title")
+$template = d.getElementById("crud-template").content
 $fragment = d.createDocumentFragment()
--- Crud-Fetch.js
+++ Crud-Fetch.js
@@ -1,6 +1,125 @@
 const d = document
 $table = d.querySelector(".crud-table")
-$form = d.querySelector(".crud-table")
-$title = d.querySelector(".crud-table")
-$template = d.getElementById(".crud-template").content
+$form = d.querySelector(".crud-form")
+$title = d.querySelector(".crud-title")
+$template = d.getElementById("crud-template").content
 $fragment = d.createDocumentFragment()
+
+const getAllSantos= async() =>{
+    try {
+        let res = await fetch("http://localhost:3000/santos")
+        let json = await res.json();
+        if(!res.ok) throw {status: res.status, statusText: res.statusText};
+        console.log(json);
+        json.forEach(el => {
+            $template.querySelector(".name").textContent= el.nombre
+            $template.querySelector(".constellation").textContent= el.constelacion
+            $template.querySelector(".edit").dataset.id= el.id
+            $template.querySelector(".edit").dataset.name= el.nombre
+            $template.querySelector(".edit").dataset.constellation= el.constelacion
+            $template.querySelector(".delete").dataset.id= el.id
+            $clone = d.importNode($template, true)
+            $fragment.appendChild($clone)
+        });
+
+
+        $table.querySelector("tbody").appendChild($fragment)
+
+    } catch (err) {
+        let message= err.statusText || "Ocurrio un Error"
+        $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
+    }
+}
+
+d.addEventListener("DOMContentLoaded",getAllSantos)
+
+d.addEventListener("submit", async e=>{
+    if(e.target===$form){
+        console.log("hola")
+        e.preventDefault()
+        if(!e.target.id.value){
+            console.log(e.target.nombre.value)
+            try {
+                let options = {
+                    method: "POST",
+                    headers:{
+                        "Content-type":"application/json; charset=utf-8"
+                    },
+                    body: JSON.stringify({
+                        nombre: e.target.nombre.value,
+                        constelacion: e.target.constelacion.value
+                    })
+
+                }
+                let res = await fetch("http://localhost:3000/santos", options)
+                json = await res.json();
+                if(!res.ok) throw {status: res.status, statusText: res.statusText};
+                location.reload()
+
+                
+            } catch (err) {
+                let message= err.statusText || "Ocurrio un Error"
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
+            }
+            
+            
+        }else{
+            try {
+                let options = {
+                    method: "PUT",
+                    headers:{
+                        "Content-type":"application/json; charset=utf-8"
+                    },
+                    body: JSON.stringify({
+                        nombre: e.target.nombre.value,
+                        constelacion: e.target.constelacion.value
+                    })
+
+                }
+                let res = await fetch(`http://localhost:3000/santos/${e.target.id.value}`, options)
+                json = await res.json();
+                if(!res.ok) throw {status: res.status, statusText: res.statusText};
+                location.reload()
+
+                
+            } catch (err) {
+                let message= err.statusText || "Ocurrio un Error"
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
+            }
+        }
+    }
+})
+
+d.addEventListener("click", async e=>{
+    if(e.target.matches(".edit")){
+        alert("Precionaste Editar")
+        $title.textContent = "Editar Santo"
+        $form.nombre.value= e.target.dataset.name
+        $form.constelacion.value= e.target.dataset.constellation
+        $form.id.value= e.target.dataset.id
+    }
+    if(e.target.matches(".delete")){
+       let isDelete= confirm("¿Estas seguro de eliminar este Caballero?")
+        if(isDelete){
+            try {
+                let options = {
+                    method: "DELETE",
+                    headers:{
+                        "Content-type":"application/json; charset=utf-8"
+                    }
+
+                }
+                let res = await fetch(`http://localhost:3000/santos/${e.target.dataset.id}`, options)
+                json = await res.json();
+                if(!res.ok) throw {status: res.status, statusText: res.statusText};
+                location.reload()
+
+                
+            } catch (err) {
+                let message= err.statusText || "Ocurrio un Error"
+                alert(`Error ${err.status}: ${message}`)
+                
+            }
+        }
+    }
+})
